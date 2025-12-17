94b09cfc91c75ba12c7caa43c8029aef858e9a68
Nourhan sabry <nourhansabry611@gmail.com>
2025-12-17 23:09:05 +0200

    Book.java

--- Book.java
+++ Book.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.mycompany.librarymanagementsystem;
 
 
@@ -48,4 +45,5 @@ public class Book {
     }
 }
     
+
 
